8ef9c260e0301f0bb7fa548d79511d6f6555a677
Denis D <65959898+vanden37@users.noreply.github.com>
2021-06-24 22:34:35 +0300

    Update and rename part_1.s to part_1.sql

--- part_1.sql
+++ part_1.sql
@@ -1,24 +1,24 @@
-1
+--1
 SELECT empls.name
 FROM employee AS empls, employee AS chieves
 WHERE chieves.id = empls.chief_id AND empls.salary > chieves.salary;
-2
+--2
 SELECT empls.name
 FROM employee AS empls
 WHERE empls.salary = (SELECT MAX(salary) FROM employees AS max
 WHERE max.department_id = empls.department_id);
-3
+--3
 SELECT department_id
 FROM employee
 GROUP BY department_id
 HAVING COUNT(department_id) <= 3;
-4
+--4
 SELECT empls.name
 FROM employee AS empls
 LEFT JOIN Employee AS chieves
 ON (empls.chief_id = chieves.Id AND empls.department_id = chieves.department_id)
 WHERE chieves.id IS NULL;
-5
+--5
 SELECT empls.department_id
 FROM employee as empls
 GROUP BY empls.department_id
